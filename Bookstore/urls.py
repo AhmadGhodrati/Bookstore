0235d158bbf7ed3acedd90f4b34dc0930f386fd4
AhmadGhodrati <ahmadghodrati021@gmail.com>
2025-02-14 13:00:04 +0330

    signup system added

--- Bookstore/urls.py
+++ Bookstore/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
         path('admin/', admin.site.urls),
         #user management
         path("accounts/", include("django.contrib.auth.urls")),
+        path("accounts/",include("accounts.urls")),
         #apps
         path("",include("pages.urls")),
 ]
